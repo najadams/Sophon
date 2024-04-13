0c4754c8f32af16066def30cd8308cddf1f4360a
najadams <najmadams1706@gmail.com>
2024-04-13 19:09:13 +0000

    some dummy css

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+  import React from "react";
+  import Card from "@mui/material/Card";
+  import CardContent from "@mui/material/CardContent";
+  import Typography from "@mui/material/Typography";
 
-const DummyCard = () => {
-  return (
-    <Card sx={{ minWidth: 400,minHeight: 400, margin: 2, flexGrow: 1 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Dummy Card
-        </Typography>
-        <Typography variant="body2">This is a dummy card.</Typography>
-      </CardContent>
-    </Card>
-  );
-};
+  const DummyCard = () => {
+    return (
+      <Card sx={{ minWidth: 400,minHeight: 400, margin: 2, flexGrow: 1 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Dummy Card
+          </Typography>
+          <Typography variant="body2">This is a dummy card.</Typography>
+        </CardContent>
+      </Card>
+    );
+  };
 
-const Dashboard = () => {
-  return (
-    <div className="page">
-      <div className="heading">
-        <h1>Dashboard</h1>
+  const Dashboard = () => {
+    return (
+      <div className="page">
+        <div className="heading">
+          <h1>Dashboard</h1>
+        </div>
+        <div className="content">
+          <DummyCard />
+          <DummyCard />
+          <DummyCard />
+          <DummyCard />
+        </div>
       </div>
-      <div className="content">
-        <DummyCard />
-        <DummyCard />
-        <DummyCard />
-        <DummyCard />
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Dashboard;
+  export default Dashboard;
