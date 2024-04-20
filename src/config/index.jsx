4bb10d7712edf484b03c8116f64293e1afcc8361
najadams <najmadams1706@gmail.com>
2024-04-20 04:46:55 +0000

    trying to add tokens for state management

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,1 +1,23 @@
-export const API_BASE_URL = "http://localhost:3001";
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:3001";
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// Request interceptor to include token with every request
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
