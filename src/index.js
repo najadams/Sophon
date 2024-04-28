c85ef71831e8e3f99edc2aa8ec290409b29022c9
najadams <najmadams1706@gmail.com>
2024-04-28 06:59:56 +0000

    good css and app working fine

--- src/index.js
+++ src/index.js
@@ -6,13 +6,32 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store, { persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 960,
+    lg: 1280,
+    xl: 1920,
+    custom: 2000, // Add your custom breakpoint
+  },
+};
+
+// Create a custom theme with the updated breakpoints
+const theme = createTheme({
+  breakpoints: breakpoints,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
